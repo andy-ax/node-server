955cb7eb296ed0d6b51adc06cc75f3e7a1462107
andy mars <andy9887776666@gmail.com>
2016-12-27 22:49:53 +0800

    Update file.js

--- bin/helper/file.js
+++ bin/helper/file.js
@@ -1,32 +1,34 @@
 var fs = require('fs');
+var buffer = require("./buffer.js");
 var FileError = require("./error.js").FileError;
 
 /**
  *
  * @param {string} filePath
  * @param {function} resolve
- * @param {?function} reject
- * @return void
+ * @param {function} [reject]
  */
-var readFileStream = function (filePath, resolve, reject) {
-    var readStream = fs.createReadStream(filePath);
-    readStream
-        .on('end', function () {
-            resolve && resolve();
-        })
-        .on('error', function (err) {
-            reject && reject(err);
-        });
+var readFile = function (filePath, resolve, reject) {
+    fs.readFile(filePath,function (err, file) {
+        if (err) {
+            if (reject) {
+                reject(err);
+            } else {
+                throw new FileError('file load failed!!!');
+            }
+        } else {
+            resolve && resolve(file);
+        }
+    });
 };
 
 /**
  *
  * @param {string} filePath
  * @param {function} resolve
- * @param {?function} reject
- * @return void
+ * @param {function} [reject]
  */
-var readFileMsg = function (filePath, resolve, reject) {
+var readFileMsg = function (filePath, resolve ,reject) {
     fs.stat(filePath, function (err, stat) {
         if (err) {
             if (reject) {
@@ -44,8 +46,8 @@ var readFileMsg = function (filePath, resolve, reject) {
  *
  * @param {string} readPath
  * @param {string} writePath
- * @param {function} resolve
- * @param {?function} reject
+ * @param {function} [resolve]
+ * @param {function} [reject]
  * @return void
  */
 var copyFile = function (readPath, writePath, resolve, reject) {
@@ -59,6 +61,45 @@ var copyFile = function (readPath, writePath, resolve, reject) {
         .pipe(fs.createWriteStream(writePath));
 };
 
-exports.readFileStream = readFileStream;
+/**
+ *
+ * @param {string} path
+ * @param {object} config
+ * @param {function} [resolve]
+ * @param {function} [reject]
+ */
+var addToFile = function (path, config, resolve, reject) {
+    if (!config.text) throw new Error('no text write!!!');
+
+    var fileText;
+    var buffers = [];
+    var encode = config.encode || 'utf-8';
+
+    fs.createReadStream(path)
+        .on('data', function (buf) {
+            buffers.push(buf);
+        })
+        .on('end', function () {
+            var fileBuf = new Buffer(config.text);
+            fileText = buffer.insertBuffer(
+                buffer.array2Buffer(buffers),
+                fileBuf,
+                config.start || 0
+            );
+            var wS = fs.createWriteStream(path);
+            wS.write(fileText, encode, function () {
+                wS.end();
+            });
+            wS.on('finish', function () {
+                resolve && resolve();
+            });
+            wS.on('error', function (err) {
+                reject && reject(err);
+            })
+        })
+};
+
+exports.readFile= readFile;
 exports.readFileMsg = readFileMsg;
 exports.copyFile = copyFile;
+exports.addToFile = addToFile;
